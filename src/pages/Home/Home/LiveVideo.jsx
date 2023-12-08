37a2bf2f798da6b7f15101936ba38a5bd6efe7a2
Suyel Haque <slsuyel@gmail.com>
2023-12-09 00:12:35 +0600

    video autoplay

--- src/pages/Home/Home/LiveVideo.jsx
+++ src/pages/Home/Home/LiveVideo.jsx
@@ -1,48 +1,8 @@
-// import React from 'react';
-// const LiveVideo = () => {
-
-
-//     return (
-//         <div className='container-fluid my-4'>
-//             <div className='d-flex justify-content-center'>
-//                 <div
-//                     style={{
-//                         position: 'relative',
-//                         overflow: 'hidden',
-//                         paddingTop: '56.25%',
-//                         width: '90%'
-//                     }}
-//                 >
-//                     <iframe
-//                         className='mx-auto'
-//                         width="1000"
-//                         height="555"
-//                         src="https://www.youtube.com/embed/dmGprO-4k5I?si=ip4D3LZfPtNAJEv0"
-//                         title="YouTube video player"
-//                         frameBorder="0"
-//                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//                         allowFullScreen
-//                         autoPlay
-//                         style={{
-//                             position: 'absolute',
-//                             top: '0',
-//                             left: '0',
-//                             width: '100%',
-//                             height: '100%'
-//                         }}
-//                     ></iframe>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default LiveVideo;
 import React, { useState } from 'react';
 
 const LiveVideo = () => {
     const [videoLoaded, setVideoLoaded] = useState(false);
-    console.log(videoLoaded);
+
     const handleVideoLoad = () => {
         setVideoLoaded(true);
     };
@@ -54,9 +14,6 @@ const LiveVideo = () => {
                     <div>
                         <p className='text-center'>Live TV Loading.... Please wait a moment</p>
                         <img src="https://cdn.dribbble.com/users/24885/screenshots/1286944/video_camera_loader.gif" alt="" />
-
-
-
                     </div>
                 )}
 
@@ -74,12 +31,8 @@ const LiveVideo = () => {
                         className='mx-auto'
                         width="1000"
                         height="555"
-                        src="https://www.youtube.com/embed/dmGprO-4k5I?si=ip4D3LZfPtNAJEv0"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowFullScreen
-                        autoPlay
+                        src="https://www.youtube.com/embed/dmGprO-4k5I?autoplay=1&amp;controls=0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+
                         style={{
                             position: 'absolute',
                             top: '0',
@@ -87,7 +40,8 @@ const LiveVideo = () => {
                             width: '100%',
                             height: '100%'
                         }}
-                    ></iframe>
+
+                        allowfullscreen></iframe>
                 </div>
             </div>
         </div>
