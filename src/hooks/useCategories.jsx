09d083233c80b3f05e009fe7cc471d97ad4c070a
slsuyel <slsuyel@gmail.com>
2024-06-07 16:16:51 +0600

    update

--- src/hooks/useCategories.jsx
+++ src/hooks/useCategories.jsx
@@ -1,15 +1,19 @@
 import { useQuery } from 'react-query';
 import { callApi } from '../utils/functions';
 
-const fetchCategories = async () => {
-    const response = await callApi("GET", `/api/categories`);
-    return response;
+const fetchCategories = async (type = 'backend') => {
+  const response = await callApi('GET', `/api/categories?type=${type}`);
+  return response;
 };
 
-const useCategories = () => {
-    const { data: categories, isLoading, refetch } = useQuery('categories', fetchCategories);
+const useCategories = type => {
+  const {
+    data: categories,
+    isLoading,
+    refetch,
+  } = useQuery('categories', () => fetchCategories(type));
 
-    return { categories, isLoading, refetch };
+  return { categories, isLoading, refetch };
 };
 
 export default useCategories;
--- src/pages/Dashboard/Blogs/AddBlog.jsx
+++ src/pages/Dashboard/Blogs/AddBlog.jsx
@@ -12,191 +12,222 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 const AddBlog = () => {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const [content, setContent] = useState('');
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const { categories, isLoading } = useCategories()
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { categories, isLoading } = useCategories('frontend');
 
-    const handleChange = (html) => {
-        console.log(html);
-        setContent(html);
-    }
-    const modules = useMemo(() => ({
-        imageResize: {
-            parchment: Quill.import('parchment'),
-        },
-        toolbar: [
-            [{ 'header': '1' }, { 'font': [] }],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            ['bold', 'italic', 'underline'],
-            ['link', 'image'],
-            [{ 'color': [] }, { 'background': [] }], [{ 'align': [] }],
-            ['clean']
-        ],
-        imageUploader: {
-            upload: (file) => {
-                return new Promise((resolve, reject) => {
-                    const formData = new FormData();
-                    formData.append("image", file);
-                    fetch(
-                        "https://api.imgbb.com/1/upload?key=334ecea9ec1213784db5cb9a14dac265",
-                        {
-                            method: "POST",
-                            body: formData
-                        }
-                    )
-                        .then(response => response.json())
-                        .then(result => {
-                            if (result.data && result.data.url) {
-                                resolve(result.data.url);
-                            } else {
-                                reject("Upload failed");
-                            }
-                        })
-                        .catch(error => {
-                            reject("Upload failed", error);
-                        });
-                });
-            }
-        }
-    }), []);
+  const handleChange = html => {
+    console.log(html);
+    setContent(html);
+  };
 
-
-    // console.log(selectedCategories);
-    const handleSubmit = async (e) => {
-        setLoading(true)
-        e.preventDefault();
-
-        const formData = new FormData();
-        formData.append('title', title);
-        formData.append('author', author);
-        formData.append('content', content);
-        formData.append('banner', selectedImage);
-        selectedCategories.forEach(category => {
-            formData.append('categories[]', category);
-        });
-
-        try {
-            const res = await callApi('post', '/api/articles', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
+  const modules = useMemo(
+    () => ({
+      imageResize: {
+        parchment: Quill.import('parchment'),
+      },
+      toolbar: [
+        [{ header: '1' }, { font: [] }],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['bold', 'italic', 'underline'],
+        ['link', 'image'],
+        [{ color: [] }, { background: [] }],
+        [{ align: [] }],
+        ['clean'],
+      ],
+      imageUploader: {
+        upload: file => {
+          return new Promise((resolve, reject) => {
+            const formData = new FormData();
+            formData.append('image', file);
+            fetch(
+              'https://api.imgbb.com/1/upload?key=334ecea9ec1213784db5cb9a14dac265',
+              {
+                method: 'POST',
+                body: formData,
+              }
+            )
+              .then(response => response.json())
+              .then(result => {
+                if (result.data && result.data.url) {
+                  resolve(result.data.url);
+                } else {
+                  reject('Upload failed');
                 }
-            });
+              })
+              .catch(error => {
+                reject('Upload failed', error);
+              });
+          });
+        },
+      },
+    }),
+    []
+  );
 
-            if (res.author) {
-                setLoading(false)
-                toast.success(' You have successfully created a news', {
-                    position: toast.POSITION.TOP_CENTER
-                });
-                navigate(`/dashboard/success-post?title=${res.title}&slug=${res.slug}`);
+  //   console.log(selectedCategories);
+  const handleSubmit = async e => {
+    setLoading(true);
+    e.preventDefault();
 
-            }
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('author', author);
+    formData.append('content', content);
+    formData.append('banner', selectedImage);
+    selectedCategories.forEach(category => {
+      formData.append('categories[]', category);
+    });
 
+    try {
+      const res = await callApi('post', '/api/articles', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
 
-        } catch (error) {
-            setLoading(false)
-            console.error('Error:', error);
-        }
-        setLoading(false)
-    };
-
-
-    if (isLoading) {
-        return <SkeletonLoader />
-    }
-
-
-    const categoryOptions = categories.reduce((acc, category) => {
-
-        acc.push({
-            value: category.id,
-            label: category.label,
-            isCategory: true
+      if (res.author) {
+        setLoading(false);
+        toast.success(' You have successfully created a news', {
+          position: toast.POSITION.TOP_CENTER,
         });
+        navigate(`/dashboard/success-post?title=${res.title}&slug=${res.slug}`);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error('Error:', error);
+    }
+    setLoading(false);
+  };
 
+  if (isLoading) {
+    return <SkeletonLoader />;
+  }
 
-        if (category.subcategories) {
-            category.subcategories.forEach(subcategory => {
-                acc.push({
-                    value: subcategory.id, // Use ID instead of name
-                    label: subcategory.label,
-                    isCategory: false // Indicates it's a subcategory
-                });
-            });
-        }
-        return acc;
-    }, []);
+  const transformCategoriesToOptions = categories => {
+    const options = [];
 
+    categories.forEach(category => {
+      options.push({
+        value: category.id,
+        label: category.label,
+      });
 
-    const handleCategoryChange = (selectedOptions) => {
-        const selectedCategoryIds = selectedOptions
-            .filter(option => option.isCategory)
-            .map(option => option.value);
-        setSelectedCategories(selectedCategoryIds);
-    };
+      if (category.children) {
+        category.children.forEach(subcategory => {
+          options.push({
+            value: subcategory.id,
+            label: `${category.label} > ${subcategory.label}`,
+          });
+        });
+      }
+    });
 
+    return options;
+  };
 
+  const categoryOptions = transformCategoriesToOptions(categories);
 
-    return (
+  const handleCategoryChange = selectedOptions => {
+    console.log(selectedOptions);
 
-        <>
-            <Form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <Label className='font-monospace fs-5 text-primary' for="title">Title:</Label>
-                    <Input className='border-success-subtle' type="text" name="title" id="title" required value={title} onChange={(e) => setTitle(e.target.value)} />
-                </FormGroup>
+    const selectedIds = selectedOptions.map(option => option.value);
+    setSelectedCategories(selectedIds);
+  };
 
-                <div className="row ">
-                    <FormGroup className='col-md-6'>
-                        <Label className='font-monospace fs-5 text-primary' for="author">Author:</Label>
-                        <Input className='border-success-subtle' type="text" name="author" id="author" required value={author} onChange={(e) => setAuthor(e.target.value)} />
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label className="font-monospace fs-5 text-primary" for="title">
+            Title:
+          </Label>
+          <Input
+            className="border-success-subtle"
+            type="text"
+            name="title"
+            id="title"
+            required
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </FormGroup>
 
-                    </FormGroup>
+        <div className="row ">
+          <FormGroup className="col-md-6">
+            <Label className="font-monospace fs-5 text-primary" for="author">
+              Author:
+            </Label>
+            <Input
+              className="border-success-subtle"
+              type="text"
+              name="author"
+              id="author"
+              required
+              value={author}
+              onChange={e => setAuthor(e.target.value)}
+            />
+          </FormGroup>
 
-                    <FormGroup className='col-md-6'>
-                        <Label className='font-monospace fs-5 text-primary' for="categories">Categories:
+          <FormGroup className="col-md-6">
+            <Label
+              className="font-monospace fs-5 text-primary"
+              for="categories"
+            >
+              Categories:
+            </Label>
 
+            <Select
+              options={categoryOptions}
+              isMulti
+              value={categoryOptions.filter(option =>
+                selectedCategories.includes(option.value)
+              )}
+              onChange={handleCategoryChange}
+              required
+            />
+          </FormGroup>
+        </div>
 
+        <FormGroup>
+          <Label className="font-monospace fs-5 text-primary" for="banner">
+            Banner:
+          </Label>
+          <Input
+            className="border-success-subtle"
+            accept="image/*"
+            name="banner"
+            id="banner"
+            type="file"
+            onChange={e => setSelectedImage(e.target.files[0])}
+            required
+          />
+        </FormGroup>
 
-                        </Label>
+        <FormGroup className="bg-white px-2">
+          <Label className="font-monospace fs-5 text-primary" for="content">
+            Content:
+          </Label>
+          <ReactQuill
+            theme="snow"
+            style={{ height: '80vh', paddingBottom: '61px' }}
+            value={content}
+            onChange={handleChange}
+            modules={modules}
+          />
+        </FormGroup>
 
-                        <Select
-                            options={categoryOptions}
-                            isMulti
-                            value={selectedCategories.map(id => ({ value: id, label: id, isCategory: true }))}
-                            onChange={handleCategoryChange}
-                            required
-                        />
-                    </FormGroup>
-                </div>
-
-                <FormGroup>
-                    <Label className='font-monospace fs-5 text-primary' for="banner">Banner:</Label>
-                    <Input className='border-success-subtle' accept="image/*" name="banner" id="banner" type='file' onChange={(e) => setSelectedImage(e.target.files[0])} required />
-                </FormGroup>
-
-                <FormGroup className='bg-white px-2'>
-                    <Label className='font-monospace fs-5 text-primary' for="content">Content:</Label>
-                    <ReactQuill theme="snow"
-                        style={{ height: '80vh', paddingBottom: '61px' }}
-                        value={content} onChange={handleChange} modules={modules} />
-                </FormGroup>
-
-                <Button disabled={loading} color="primary" className='' type="submit">
-                    Submit
-                </Button>
-            </Form>
-
-
-
-        </>
-
-
-    );
+        <Button disabled={loading} color="primary" className="" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default AddBlog;
