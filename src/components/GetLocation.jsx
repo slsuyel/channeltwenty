db082e240e235eca5057f45ab65db591b06f94ad
slsuyel <slsuyel@gmail.com>
2024-06-07 17:55:33 +0600

    up

--- src/components/GetLocation.jsx
+++ src/components/GetLocation.jsx
@@ -23,7 +23,8 @@ const GetLocation = () => {
           }
         }, showError);
       } else {
-        alert('Geolocation is not supported by this browser.');
+        console.log('Geolocation is not supported by this browser.');
+        // alert('Geolocation is not supported by this browser.');
       }
     };
 
@@ -33,19 +34,19 @@ const GetLocation = () => {
   const showError = error => {
     switch (error.code) {
       case error.PERMISSION_DENIED:
-        alert('User denied the request for Geolocation.');
+        console.log('User denied the request for Geolocation.');
         break;
       case error.POSITION_UNAVAILABLE:
-        alert('Location information is unavailable.');
+        console.log('Location information is unavailable.');
         break;
       case error.TIMEOUT:
-        alert('The request to get user location timed out.');
+        console.log('The request to get user location timed out.');
         break;
       case error.UNKNOWN_ERROR:
-        alert('An unknown error occurred.');
+        console.log('An unknown error occurred.');
         break;
       default:
-        alert('An unknown error occurred.');
+        console.log('An unknown error occurred.');
     }
   };
 
